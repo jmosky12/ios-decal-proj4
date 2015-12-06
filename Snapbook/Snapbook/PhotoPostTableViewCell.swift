886ab21d2fc79192ba9c48954cacac9c4ba19a96
Jake Moskowitz <jmoskowitz12@berkeley.edu>
2015-12-05 22:00:04 -0800

    Pre Login Merge

---
+++ Snapbook/Snapbook/PhotoPostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoPostTableViewCell.swift
+//  Snapbook
+//
+//  Created by Jake Moskowitz on 12/5/15.
+//  Copyright © 2015 Jake Moskowitz. All rights reserved.
+//
+
+import UIKit
+
+class PhotoPostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
